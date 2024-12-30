958a150a58671c70b98f5fb57305f9e1b36cd260
Javas <javasaryadinata24@gmail.com>
2024-12-30 10:07:41 +0700

    Update DB

--- sql/uas_pemweb.sql
+++ sql/uas_pemweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2024 at 08:27 AM
+-- Generation Time: Dec 30, 2024 at 04:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,9 @@ INSERT INTO `admins` (`id`, `username`, `nama`, `password`) VALUES
 ('admin-1717757076', 'nabil', 'muhammad nabil', '827ccb0eea8a706c4c34a16891f84e7b'),
 ('admin-1729315010', 'javas', 'javas', '827ccb0eea8a706c4c34a16891f84e7b'),
 ('admin-1735374692', 'daoa', 'Jorge Martin', '21232f297a57a5a743894a0e4a801fc3'),
-('admin-1735392857', 'japaz', 'Deva', '6efc67e68005e7503df580d11e5e7a23');
+('admin-1735392857', 'japaz', 'Deva', '6efc67e68005e7503df580d11e5e7a23'),
+('admin-1735465072', 'javast', 'Javas Uhuy', 'd8578edf8458ce06fbc5bb76a58c5ca4'),
+('admin-1735488973', 'Username', 'Nama', 'dc647eb65e6711e155375218212b3964');
 
 -- --------------------------------------------------------
 
@@ -69,12 +71,11 @@ CREATE TABLE `produk_gitar` (
 --
 
 INSERT INTO `produk_gitar` (`id`, `nama`, `harga`, `deskripsi`, `image_url`, `stok`, `admin_id`, `created_at`, `updated_at`) VALUES
-(10, 'Gitar Ovation', 2750000.00, 'Bejirr', 'assets/guitar/10.-Gitar-Akustik-Ovation-420x42.jpg', 2, 'admin-1735374692', '2024-12-28 12:42:42', '2024-12-29 06:08:43'),
-(11, 'Gitar', 1000000.00, 'Tes', 'assets/guitar/704087_720.png', 3, 'admin-1729315010', '2024-12-28 12:56:41', '2024-12-29 05:47:55'),
-(14, 'PIA3761', 56800000.00, 'Designed in collaboration with Steve Vai, the Ibanez Steve Vai PIA3761 is a sonically versatile, high-performance solidbody electric guitar. ', 'assets/guitar/p_region_PIA3761_SLW_00_02_sub_1.jpg', 5, 'admin-1729315010', '2024-12-28 17:28:45', '2024-12-29 05:48:10'),
+(10, 'Yahaha', 2000000.00, 'Baru nih', 'assets/guitar/10.-Gitar-Akustik-Ovation-420x42.jpg', 1, 'admin-1735374692', '2024-12-28 12:42:42', '2024-12-30 00:09:07'),
 (15, 'Les Paul Traditional Pro V', 48600000.00, 'The Gibson Les Paul Traditional Pro V AAA Flame Top is a modern upgrade to a classic design.', 'assets/guitar/L69587000007000-00-600x600.jpg', 3, 'admin-1729315010', '2024-12-28 17:41:18', '2024-12-29 05:48:18'),
-(16, 'Fender Player Stratocaster', 11300000.00, 'The Fender Player Limited-Edition Stratocaster an electric guitar built for a pro with an entry-level price tag. ', 'assets/guitar/L46833000001000-00-600x600.jpg', 6, 'admin-1729315010', '2024-12-28 17:42:58', '2024-12-29 07:16:45'),
-(18, 'Schecter Synyster Gates Custom-7', 37200000.00, 'At long last, the next entry in the signature Synyster Gates garrison of guitars has landed: the Schecter Custom-7 TR Headless.', 'assets/guitar/Schecter_Syn.jpg', 4, 'admin-1729315010', '2024-12-29 05:35:15', '2024-12-29 07:18:05');
+(16, 'Fender Player Stratocaster Black', 11300000.00, 'The Fender Player Limited-Edition Stratocaster an electric guitar built for a pro with an entry-level price tag. ', 'assets/guitar/L46833000001000-00-600x600.jpg', 4, 'admin-1729315010', '2024-12-28 17:42:58', '2024-12-29 14:18:22'),
+(18, 'Schecter Synyster Gates Custom-7', 37200000.00, 'At long last, the next entry in the signature Synyster Gates garrison of guitars has landed: the Schecter Custom-7 TR Headless.', 'assets/guitar/Schecter_Syn.jpg', 4, 'admin-1729315010', '2024-12-29 05:35:15', '2024-12-29 07:18:05'),
+(29, 'Fender Telecaster', 4000000.00, 'Fender bagus nih bos', 'assets/guitar/704087_720.png', 5, '', '2024-12-30 02:46:20', '2024-12-30 02:51:00');
 
 --
 -- Indexes for dumped tables
@@ -101,7 +102,7 @@ ALTER TABLE `produk_gitar`
 -- AUTO_INCREMENT for table `produk_gitar`
 --
 ALTER TABLE `produk_gitar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
